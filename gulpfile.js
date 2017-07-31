db5d576c5edb9a86847497cfd826ba6b15571111
Raquel Bujans <raquel.bujans@gmail.com>
2017-07-31 03:41:27 -0400

    trying a gulp

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp        = require('gulp');
+var deploy      = require('gulp-gh-pages');
+
+// TODO: simple browserify bundle task here
+
+/**
+ * Push build to gh-pages
+ */
+// gulp.task('deploy', function () {
+//   return gulp.src("./dist/**/*")
+//     .pipe(deploy())
+// });
